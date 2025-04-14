3e6bf451298762a35e0a8d985033dcd378814864
sypovik <nekich0999@gmail.com>
2025-04-15 01:07:08 +0700

    Add Air configuration and refactor main function for improved database setup

--- run.go
+++ run.go
@@ -1,60 +1,31 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"go-restapi/internal/config"
 	"go-restapi/internal/handler"
 	"go-restapi/internal/repository"
 	"go-restapi/internal/service"
-	"log"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
-func conectToDB() (*sql.DB, error) {
-	config := config.LoadConfig()
-
-	db, err := sql.Open("postgres", config.DBUrl)
-	if err != nil {
-		log.Fatalf("Ошибка создания объекта БД: %v", err) // Переименуем сообщение
-	}
-
-	Timeout, err := time.ParseDuration(config.DBTimeout)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), Timeout)
-	defer cancel()
-
-	if err := db.PingContext(ctx); err != nil {
-		log.Fatalf("Ошибка подключения к БД: %v", err) // Здесь будет реальная ошибка
-	}
-	log.Println("Подключение к БД успешно")
-	return db, nil
-}
-
-func conectToRepository() repository.NoteRepository {
-	db, _ := conectToDB()
-	return repository.NewNoteRepository(db)
-}
-
 func main() {
 	db, _ := repository.SetupDatabase()
-	var dr repository.NoteRepository = repository.NewNoteRepository(db)
-	var sr service.NoteService = service.NewNoteService(dr)
-	var hand handler.NoteHandler = handler.NewNoteHandler(sr)
-	// Правильно сохраняем и используем функцию отмены
+	var repo repository.NoteRepository = repository.NewNoteRepository(db)
+	var serv service.NoteService = service.NewNoteService(repo)
+	var hand handler.NoteHandler = handler.NewNoteHandler(serv)
+	// gin.SetMode(gin.ReleaseMode)
 
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.SetTrustedProxies(nil)
+
 	hand.RegisterRoutes(r)
 
 	r.GET("/favicon.ico", func(c *gin.Context) {
 		c.Status(204) // No Content
 	})
 
-	r.Run(":8083") // Запускаем сервер на порту 8083
+	r.Run(":8083")
 }
